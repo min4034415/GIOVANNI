d09cf743a7370abbeac26b6e7e4360f6489098de
min4034415 <min4034415@gmail.com>
2025-04-21 15:23:30 +0900

    AsOfPg11

---
+++ src/MultiInterfaceImplExample.java
@@ -0,0 +1,9 @@
+public class MultiInterfaceImplExample {
+  public static void main(String[] args) {
+    RemoteControl rc = new SmartTelevision();
+        rc.turnOn();
+        rc.turnOff();
+  Searchable sc = new SmartTelevision();
+    sc.search("HIHIH");
+  };
+}
---
+++ src/SmartTelevision.java
@@ -0,0 +1,15 @@
+public class SmartTelevision implements RemoteControl, Searchable{
+    public void turnOff() {
+        System.out.println("Turining OFf");
+    }
+    public void setVolume(int volume) {
+        System.out.println("Setting volume to: " + volume);
+    }
+    public void turnOn() {
+        System.out.println("Turinigni on");
+    }
+    public void search(String url){
+        System.out.println(url);
+    }
+    
+}
